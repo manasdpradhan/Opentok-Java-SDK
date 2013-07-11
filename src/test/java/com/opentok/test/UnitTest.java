ac3c3683dce9c21fc52525ba6cf78c711c31bfed
Manas <manas@tokbox.com>
2013-07-11 11:48:13 -0700

    Enforced using correct sessionIds while generating tokens.

--- src/test/java/com/opentok/test/UnitTest.java
+++ src/test/java/com/opentok/test/UnitTest.java
@@ -28,8 +28,8 @@ public class UnitTest {
     private String apiSecret;
 
     public UnitTest() {
-        apiKey = Integer.valueOf(System.getProperty("apiKey"));
-        apiSecret = System.getProperty("apiSecret");
+        apiKey = 1127;
+        apiSecret = "f92f49ece2ac828587da9266858578e4";
 		sdk = new OpenTokSDK(apiKey, apiSecret);
     }
 
@@ -183,7 +183,7 @@ public class UnitTest {
     	OpenTokException expected = null;
 		try {
             String s= sdk.create_session().session_id;
-            String t = sdk.generate_token(s, "asdfasdf");
+            sdk.generate_token(s, "asdfasdf");
 		} catch (OpenTokException e) {
 			expected = e;
         }
@@ -202,6 +202,39 @@ public class UnitTest {
 		Assert.assertNotNull("Java SDK tests: exception should be thrown for role null", expected);
     }
     
+    @Test
+    public void testTokenNullSessionId() throws OpenTokException {
+        OpenTokException expected = null;
+        try {
+            sdk.generate_token(null);
+        } catch (OpenTokException e) {
+            expected = e;
+        }
+        Assert.assertNotNull("Java SDK tests: exception should be thrown for null sessionId", expected);
+    }
+    
+
+    public void testTokenEmptySessionId() throws OpenTokException {
+        OpenTokException expected = null;
+        try {
+            sdk.generate_token("");
+        } catch (OpenTokException e) {
+            expected = e;
+        }
+        Assert.assertNotNull("Java SDK tests: exception should be thrown for null sessionId", expected);
+    }
+
+    @Test
+    public void testTokenIncompleteSessionId() throws OpenTokException {
+        OpenTokException expected = null;
+        try {
+            sdk.generate_token("jkasjda2ndasd");
+        } catch (OpenTokException e) {
+            expected = e;
+        }
+        Assert.assertNotNull("Java SDK tests: exception should be thrown for null sessionId", expected);
+    }
+
     @Test
     public void testTokenExpireTimeDefault() throws OpenTokException {
         String s= sdk.create_session().session_id;
